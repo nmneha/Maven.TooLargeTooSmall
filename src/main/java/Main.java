f51bd3859c828b8ce9688fee387d23497ef0603b
Nusera <nusera@zipcoders-MBP.fios-router.home>
2022-03-02 23:03:47 -0500

    cleaned up code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,29 +9,28 @@ public class Main {
         int random = (int) (Math.random() * (100 - 1) + 1);
         Scanner scanner = new Scanner(System.in);
         Integer userInput = 0;
-        int attempts = 0;
-        int memory = userInput;
+        int attempts = 0,
+                memory = 0;
         Main.println("Enter a number between 1 and 100.");
         while (userInput != random) {
             userInput = scanner.nextInt();
-                if (userInput == memory) {
-                  System.out.println("You already said that. Try again");
-                  memory = userInput;
-                } else if (userInput > random) {
-                    System.out.print("Too large.");
-                    attempts += 1;
-                    memory = userInput;
-                } else if (userInput < random) {
-                    System.out.print("Too small.");
-                    attempts += 1;
-                    memory = userInput;
-                } else if (userInput == random) {
-                    System.out.print("Correct guess.");
-                    attempts += 1;
-                    memory = userInput;
-                }
+            if (userInput == memory) {
+                System.out.println("You already said that. Try again");
+            } else if (userInput > random) {
+                System.out.println("Too large.");
+                attempts += 1;
+                memory = userInput;
+            } else if (userInput < random) {
+                System.out.println("Too small.");
+                attempts += 1;
+                memory = userInput;
+            } else if (userInput == random) {
+                System.out.println("Correct guess.");
+                attempts += 1;
+                memory = userInput;
+            }
         }
-            System.out.print("Total attempts: " + attempts);
+        System.out.print("Total attempts: " + attempts);
     }
 
     public static void print(String output, Object... args) {
@@ -40,13 +39,6 @@ public class Main {
 
     public static void println(String output, Object... args) {
         print(output + "\n", args);
-    }
-
-    public static Integer getIntegerInput(String prompt) {
-        Scanner scanner = new Scanner(System.in);
-        println(prompt);
-        Integer userInput = scanner.nextInt();
-        return userInput;
     }
 
 }
